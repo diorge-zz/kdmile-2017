c8b38b9b231ecdcd60ab77c66b950b15e7451d3f
Mirandatz <hardly.a.challenge@gmail.com>
2017-08-11 01:17:14 -0300

    Wrote some to code to run a few clustering algorithms

---
+++ src/miranda_run_algos.py
@@ -0,0 +1,114 @@
+import numpy as np
+from sklearn.cluster import *
+from sklearn.metrics import make_scorer
+from sklearn.metrics import adjusted_rand_score
+
+
+def run_kmeans(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    for k in np.arange(2, element_count):
+        labels_pred = KMeans(n_clusters=k, init='k-means++', n_init=50) \
+            .fit_predict(X, None)
+        score = calculate_score(labels_true=y, labels_pred=labels_pred)
+        scores_and_params.append((score, f'k: {k}'))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def run_agglomerative_clustering(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    for n_clusters in np.arange(2, element_count):
+        for linkage in ["ward", "complete", "average"]:
+            labels_pred = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage) \
+                .fit_predict(X, None)
+            score = calculate_score(labels_true=y, labels_pred=labels_pred)
+            scores_and_params.append((score, f'n_cluster: {n_clusters}, linkage: {linkage}'))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def run_dbscan(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    for min_samples in np.arange(2, element_count):
+        for eps in np.linspace(0.01, 0.95, 100):
+            labels_pred = DBSCAN(eps=eps, min_samples=min_samples) \
+                .fit_predict(X, None)
+            score = calculate_score(labels_true=y, labels_pred=labels_pred)
+            scores_and_params.append((score, f'min_samples: {min_samples}, eps: {eps}'))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def run_mean_shift(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    for bandwidth in np.linspace(0.05, 0.95, 50):
+        labels_pred = MeanShift(bandwidth=bandwidth) \
+            .fit_predict(X, None)
+        score = calculate_score(labels_true=y, labels_pred=labels_pred)
+        scores_and_params.append((score, f'bandwidth: {bandwidth}'))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def run_affinity_propagation(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    preferences = []
+    for i in range(2, element_count - 1):
+        temp = np.zeros(element_count)
+        for j in range(i):
+            temp[j] = 100
+        preferences.append(temp)
+
+    for damping in np.linspace(start=0.5, stop=0.95, num=50):
+        for preference in preferences:
+            labels_pred = AffinityPropagation(damping=damping, preference=preference) \
+                .fit_predict(X, None)
+            score = calculate_score(labels_true=y, labels_pred=labels_pred)
+            scores_and_params.append((score, f'damping: {damping}, preference: {preference}'))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def run_birch(X, y):
+    element_count, feature_count = X.shape
+    scores_and_params = []
+
+    for n_clusters in np.arange(2, element_count):
+        for branching_factor in np.arange(2, element_count):
+            for threshold in np.linspace(start=0.5, stop=0.95, num=50):
+                labels_pred = Birch(n_clusters=int(n_clusters), branching_factor=branching_factor, threshold=threshold) \
+                    .fit_predict(X, None)
+                score = calculate_score(labels_true=y, labels_pred=labels_pred)
+                params = f'n_clusters: {n_clusters},' \
+                         f' branching_factor: {branching_factor},' \
+                         f' threshold: {threshold}'
+            scores_and_params.append((score, params))
+
+    return get_max_score_and_params(scores_and_params)
+
+
+def get_max_score_and_params(scores_and_params):
+    max_score = None
+    max_params = None
+    for score, params in scores_and_params:
+        if max_score == None or max_score < score:
+            max_score = score
+            max_params = params
+    return max_score, max_params
+
+
+def calculate_score(labels_true, labels_pred):
+    score = adjusted_rand_score(labels_true=labels_true, labels_pred=labels_pred)
+    # Normalizing
+    # score = (score + 1) / 2
+    return score
---
+++ src/mirandatz_main.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+from miranda_run_algos import *
+
+# Constants
+DATASET_DIR = r"../data/matrices/"
+LABELS_DIR = r"../data/labels/"
+MATRICES_INDEXES = [46, 271, 368]
+COLUMN_DELIMITER = "\t"
+OUTPUT_FILE = None
+
+
+def main():
+    datasets = acquire_datasets()
+    labels = acquire_labels()
+
+    for _ in range(100):
+        for matrix_index in reversed(MATRICES_INDEXES):
+            X = datasets[matrix_index]
+            y = labels[matrix_index]['label']
+            with open(f'out_{matrix_index}.csv', mode='a', encoding='utf8') as file:
+                # Writing header
+                file.write(f'method_name{COLUMN_DELIMITER}score{COLUMN_DELIMITER}params\n')
+
+                # Running all the clustering algorithms
+                print(f'Processing dataset: {matrix_index}')
+                print(f'Dataset shape: {X.shape}')
+                pipeline(X=X, y=y, output_channel=file)
+                print()
+                file.write('\n')
+
+
+def pipeline(X, y, output_channel):
+    fiveMeansLabels = KMeans(n_clusters=5, init='k-means++', n_init=50, max_iter=300).fit_predict(X, None)
+    fiveMeansScore = calculate_score(labels_true=y, labels_pred=fiveMeansLabels)
+    print(f'5-Means: {fiveMeansScore}')
+    output_channel.write(f'five_means{COLUMN_DELIMITER}{fiveMeansScore}{COLUMN_DELIMITER}k=5\n')
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='RandomLabels',
+                           score_and_params=
+                           [
+                               calculate_score(y, np.random.random_integers(low=0, high=X.shape[0], size=len(y))),
+                               'np.random.random_integers(low=0, high=X.shape[0], size=len(y))'
+                           ]
+                           )
+
+    # print_score_and_params(fiveMeansScore=fiveMeansScore,
+    #                        output_channel=output_channel,
+    #                        method_name='Birch',
+    #                        score_and_params=run_birch(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='KMeans',
+                           score_and_params=run_kmeans(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='Birch',
+                           score_and_params=run_birch(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='AgglomerativeClustering',
+                           score_and_params=run_agglomerative_clustering(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='DBScan',
+                           score_and_params=run_dbscan(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='MeanShift',
+                           score_and_params=run_mean_shift(X, y))
+
+    print_score_and_params(fiveMeansScore=fiveMeansScore,
+                           output_channel=output_channel,
+                           method_name='AffinityPropagation',
+                           score_and_params=run_affinity_propagation(X, y))
+
+
+def print_score_and_params(fiveMeansScore, method_name, score_and_params, output_channel):
+    score, params = score_and_params
+    output_channel.write(f'{method_name}{COLUMN_DELIMITER}{score}{COLUMN_DELIMITER}{params}\n')
+    if (score > fiveMeansScore):
+        print(f'+++ {method_name} | score: {score} | {params}')
+    else:
+        print(f'--- {method_name} | score: {score} | {params}')
+
+
+def acquire_datasets():
+    datasets = {}
+    for index in MATRICES_INDEXES:
+        filename = f'{DATASET_DIR}{index}'
+        matrix = np.genfromtxt(fname=filename)
+        datasets[index] = matrix
+
+    return datasets
+
+
+def acquire_labels():
+    labels = {}
+    for index in MATRICES_INDEXES:
+        filename = f'{LABELS_DIR}{index}.csv'
+        temp = pd.read_csv(filepath_or_buffer=filename)
+        labels[index] = temp
+
+    return labels
+
+
+if __name__ == "__main__":
+    main()
+    print("DONE!")
